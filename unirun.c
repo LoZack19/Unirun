a79b22851dfbe884ebc682d43d725539d0aad911
LoZack19 <giozaccaria02@gmail.com>
2021-04-05 23:39:40 +0200

    better styling

--- unirun.c
+++ unirun.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
 #include <errno.h>
 
 #include <sys/types.h>
@@ -10,110 +9,97 @@
 
 #include "unirun.h"
 
-void empty(char* buffer, size_t size)
+char *run(char *program, char *args[], int *status)
 {
-    for(int i = 0; i < size; ++i)
-        buffer[i] = 0;
+	pid_t pid;
+	int tmp = 0;
+	int pipe_fd[2];
+	char *buffer;
+
+	if (!~pipe(pipe_fd)) {
+		perror("Failed to open the pipe");
+		return NULL;
+	}
+
+	pid = fork();
+	if (!~pid) {
+		perror("Failed to fork");
+		return NULL;
+	} else if (!pid) {
+		close(pipe_fd[0]);
+		if (!~dup2(pipe_fd[1], STDOUT_FILENO)) {
+			perror("Failed to redirect stdout to the pipe");
+			exit(-1);
+		}
+		close(pipe_fd[1]);
+
+		if (!~execv(program, args)) {
+			fprintf(stderr, "Failed to execute %s: %s",
+				program, strerror(errno));
+			exit(-1);
+		}
+	}
+
+	close(pipe_fd[1]);
+	buffer = (char *) malloc(BUFFER_SIZE);
+	memset(buffer, 0, BUFFER_SIZE);
+
+	if (!~read(pipe_fd[0], buffer, BUFFER_SIZE))
+		perror("Failed to read from the pipe");
+
+	buffer = realloc(buffer, strlen(buffer) + 1);
+	buffer[strlen(buffer)] = 0;
+	close(pipe_fd[0]);
+	wait(&tmp);
+
+	if (status && WIFEXITED(tmp))
+		*status = WEXITSTATUS(tmp);
+	return buffer;
 }
 
-char* run(char* program, char* args[], int* status)
+uint8_t launch(char *program, char *args[], int fd)
 {
-    pid_t pid;
-    int tmp = 0;
-    int pipe_fd[2];
-    char* buffer;
+	int wstatus = 0;
+	uint8_t exit_status = 0xFF;
 
-    if(!~pipe(pipe_fd)) {
-        perror("Failed to open the pipe");
-        return NULL;
-    }
+	switch (fork()) {
+	case -1:
+		perror("Failed to fork");
+		break;
 
-    pid = fork();
-    if(!~pid) {
-        perror("Failed to fork");
-        return NULL;
-    }
-    else if(!pid) {
-        //child
-        close(pipe_fd[0]);
-        if(!~dup2(pipe_fd[1], STDOUT_FILENO)) {
-            perror("Failed to redirect stdout to the pipe");
-            exit(-1);
-        }
-        close(pipe_fd[1]);
+	case 0:
+		if (fd && ~fd && fd != STDOUT_FILENO) {
+			if (!~dup2(fd, STDOUT_FILENO))
+				perror("Failed to redirect stdout");
+			if (!~close(fd))
+				perror("Failed to close file descriptor");
+		}
+		if (!~execvp(program, args))
+			fprintf(stderr, "Failed to execute %s: %s",
+				program, strerror(errno));
+		exit(-1);
 
-        if(!~execv(program, args)) {
-            fprintf(stderr, "Failed to execute %s: %s", program, strerror(errno));
-            exit(-1);
-        }
-    }
+	default:
+		wait(&wstatus);
+		if (WIFEXITED(wstatus))
+			exit_status = WEXITSTATUS(wstatus);
+		else
+			exit_status = 0xFF;
+		break;
+	}
 
-    close(pipe_fd[1]);
-    buffer = (char*) malloc(BUFFER_SIZE);
-    empty(buffer, strlen(buffer));
-
-    if(!~read(pipe_fd[0], buffer, BUFFER_SIZE))
-        perror("Failed to read from the pipe");
-    
-    buffer = realloc(buffer, strlen(buffer)+1);
-    buffer[strlen(buffer)] = 0;
-    close(pipe_fd[0]);
-    wait(&tmp);
-
-    if(status && WIFEXITED(tmp))
-        *status = WEXITSTATUS(tmp);
-    return buffer;
+	return exit_status;
 }
 
-int launch(char* program, char* args[], int fd)
+char **genargs(size_t size, ...)
 {
-    int status;
+	va_list _arg_list;
+	char **args = calloc(size, sizeof(char *));
 
-    switch (fork())
-    {
-    case -1:
-        perror("Failed to fork");
-        break;
+	va_start(_arg_list, size);
+	for (int i = 0; i < size; ++i)
+		args[i] = va_arg(_arg_list, char *);
 
-    case 0:
-        /* child */
-        // 1. Redirecting stdout
-        if(fd && ~fd && fd != STDOUT_FILENO) {
-            if(!~dup2(fd, STDOUT_FILENO)) {
-                perror("Failed to redirect stdout to the provided file descriptor");
-                fprintf(stderr, "Printing to the old stdout...\n");
-            }
-            if(!~close(fd))
-                perror("Failed to close file descriptor");
-        }
-
-        // Executing program (with execvp)
-        if(!~execvp(program, args))
-            fprintf(stderr, "Failed to execute %s: %s", program, strerror(errno));
-        exit(-1);
-    
-    default:
-        // extracting return value
-        wait(&status);
-        if(WIFEXITED(status))
-            status = WEXITSTATUS(status);
-        else
-            status = -1; // WEXITYSTATUS can't return (int)(-1)
-        break;
-    }
-
-    return status;
-}
-
-char** genargs(size_t size, ...)
-{
-    va_list _arg_list;
-    char** args = (char**) calloc(size, sizeof(char**));
-
-    va_start(_arg_list, size);
-    for(int i = 0; i < size; ++i)
-        args[i] = va_arg(_arg_list, char*);
-    
-    va_end(_arg_list);
-    return args;
+	va_end(_arg_list);
+	return args;
 }
--- unirun.h
+++ unirun.h
@@ -3,13 +3,14 @@
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <stddef.h>
 
 #define BUFFER_SIZE     0x4000
 #define PATH_SIZE       0x400
 
 char* run(char* program, char* args[], int* status);    // returns the output in a buffer
-int launch(char* program, char* args[], int fd);        // prints the output to fd (if fd is 0 doesn't redirect)
+uint8_t launch(char* program, char* args[], int fd);    // prints the output to fd (if fd is 0 doesn't redirect)
 char** genargs(size_t size, ...);                       // generates an array of arguments from a list
 
 #define WHICH(_program)             run("/usr/bin/which", genargs(3, "/usr/bin/which", (_program), NULL), NULL)
